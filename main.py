225680059164da92eb5132b55ea176221983ba0f
Emmet Murray <ehmurray8@gmail.com>
2018-08-21 19:09:32 -0400

    Cleaned up main.

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import time
 import json
 
 
+def create_watson_json_from_twitter(screen_name: str) -> str:
+    tweet_objects = get_tweets_string(screen_name)
+    items = tweets_to_content_items(tweet_objects)
+    watson_json_string = '{"contentItems": ['
+    watson_json_string += ",".join([json.dumps(item.__dict__) for item in items])
+    return watson_json_string
+
 def get_tweets_string(screen_name: str) -> namedtuple:
     oauth = OAuth(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
     twitter = Twitter(auth=oauth)
@@ -25,8 +32,5 @@ def tweets_to_content_items(tweets: List[Dict]) -> List[ContentItem]:
     return content_items
 
 if __name__ == "__main__":
-    tweet_objects = get_tweets_string("realDonaldTrump")
-    items = tweets_to_content_items(tweet_objects)
-    watson_json_string = '{"contentItems": ['
-    watson_json_string += ",".join([json.dumps(item.__dict__) for item in items])
-    print(watson_json_string)
+    json_string = create_watson_json_from_twitter("realDonaldTrump")
+    print(json_string)
